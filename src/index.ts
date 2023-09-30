233f13d0a6b9c887f71167410778131ddcaa62cd
Kaan Yagci <kaan.yagci@makepad.fr>
2023-09-30 21:08:32 +0200

    refactor(index): downloading changed to sequantially from concurrently
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,10 @@ async function main(headless: boolean) {
   await cleanDownloader(config);
   console.log(`Number of track links: ${trackLinks.length}`);
   const tracks = [];
-  console.log(trackLinks);
   for (const trackLink of trackLinks) {
-    console.debug(`Current track link ${trackLink}`);
     const track = await downloadTrack({ baseURL: trackLink, headless });
-    console.log('Track');
-    console.log(track);
     tracks.push(track);
   }
-  // const tracks = await Promise.all(
-  //   trackLinks.map(async (trackLink) => {
-  //     const track = await
-  //     return track;
-  //   }),
-  // );
   console.debug('Tracks');
   console.log(JSON.stringify(tracks, undefined, 4));
 }
