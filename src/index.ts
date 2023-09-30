d9f0f68c8a3c1b3dac2c489523f7b5f39600999d
Kaan Yagci <kaan.yagci@makepad.fr>
2023-09-30 15:40:55 +0200

    feat(index): download tracks
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
-import { getTrackLinks } from './chosic-downloader';
+import {
+  cleanDownloader,
+  downloadTrack,
+  getTrackLinks,
+} from './chosic-downloader';
 
 async function main(headless: boolean) {
-  const tracklinks = await getTrackLinks({
+  const { config, trackLinks } = await getTrackLinks({
     baseURL: 'https://www.chosic.com/free-music/lofi',
-    headless: false,
+    headless: headless,
   });
-  console.log(`Number of track links: ${tracklinks.length}`);
-  //   TODO: Download the track
-  //  TODO: Get track information
+  await cleanDownloader(config);
+  console.log(`Number of track links: ${trackLinks.length}`);
+  const tracks = [];
+  console.log(trackLinks);
+  for (const trackLink of trackLinks) {
+    console.debug(`Current track link ${trackLink}`);
+    const track = await downloadTrack({ baseURL: trackLink, headless });
+    console.log('Track');
+    console.log(track);
+    tracks.push(track);
+  }
+  // const tracks = await Promise.all(
+  //   trackLinks.map(async (trackLink) => {
+  //     const track = await
+  //     return track;
+  //   }),
+  // );
+  console.debug('Tracks');
+  console.log(JSON.stringify(tracks, undefined, 4));
 }
 
-main(false).then();
+main(true).then();
