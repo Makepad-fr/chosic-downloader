c3388f697ec9b8f58b9a4ad25354fe35a13d42c1
Kaan Yagci <kaan.yagci@makepad.fr>
2023-09-30 15:40:26 +0200

    tests: First unit tests added wih jest
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

---
+++ __tests__/example.test.ts
@@ -0,0 +1,12 @@
+import { downloadTrack } from "../src/chosic-downloader";
+
+const TRACK_URL = 'https://www.chosic.com/download-audio/45401/';
+
+test('Get track details', async () => {
+    const track = await  downloadTrack({
+        baseURL: TRACK_URL,
+        headless: false,
+    });
+    expect(track.info.title.trim()).toBe('Purple Dream');
+    expect(track.info.artistName.trim()).toBe('Ghostrifter Official');
+  }, 100000);
